9e6408d13005aace6c0a3f9fe2c13f61a3e52245
huy17212 <hahuytri2K3@gmail.com>
2023-06-04 01:53:12 +0700

    commit

--- src/main/java/com/HTT/company/service/Impl/FilesStorageServiceImpl.java
+++ src/main/java/com/HTT/company/service/Impl/FilesStorageServiceImpl.java
@@ -23,8 +23,8 @@ public class FilesStorageServiceImpl implements JavaFileStorageService {
 	@Override
 	public void save(MultipartFile file) {
 		try {
-			System.out.print("Kaka1 " + file);
-			System.out.print("Kaka2 " + root);
+			System.out.println("Kaka1 " + file);
+			System.out.println("Kaka2 " + root);
 			Files.copy(file.getInputStream(), this.root.resolve(file.getOriginalFilename()));
 		} catch (Exception e) {
 			e.printStackTrace();
