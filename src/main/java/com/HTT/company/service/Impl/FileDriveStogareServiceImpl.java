7bf5a132b4c953c901a5daa5a1c1dab50ceb10a4
huy17212 <hahuytri2K3@gmail.com>
2023-06-05 00:59:42 +0700

    first

--- src/main/java/com/HTT/company/service/Impl/FileDriveStogareServiceImpl.java
+++ src/main/java/com/HTT/company/service/Impl/FileDriveStogareServiceImpl.java
@@ -173,7 +173,7 @@ public class FileDriveStogareServiceImpl implements JavaFileDriveStogareService 
 
         // Create a FileContent object with the image file
         System.out.println("kaka save me " + System.getenv("HOME"));
-        java.io.File file = new java.io.File(System.getenv("HOME") + "C:\\Users\\Huy1721\\Desktop\\web-start-training2\\anh4.jpg");
+        java.io.File file = new java.io.File(System.getenv("HOME") + "/C:\\Users\\Huy1721\\Desktop\\web-start-training2\\anh4.jpg");
         FileContent mediaContent = new FileContent("image/jpeg", file);
 
         // Upload the file
