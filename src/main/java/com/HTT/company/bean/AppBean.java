0507a478c7e6823615b0a125fb4dc61db734447c
huy17212 <hahuytri2K3@gmail.com>
2023-06-09 04:24:24 +0700

    fiest

--- src/main/java/com/HTT/company/bean/AppBean.java
+++ src/main/java/com/HTT/company/bean/AppBean.java
@@ -1,9 +1,6 @@
 package com.HTT.company.bean;
 
-import java.util.HashMap;
-import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -11,55 +8,26 @@ import org.springframework.security.web.authentication.rememberme.InMemoryTokenR
 import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 
 import com.HTT.company.entity.Users;
-import com.paypal.base.rest.APIContext;
-import com.paypal.base.rest.OAuthTokenCredential;
-import com.paypal.base.rest.PayPalRESTException;
 
 @Configuration
 public class AppBean {
-	
-	@Value("${paypal.client.id}")
-	private String clientId;
-	@Value("${paypal.client.secret}")
-	private String clientSecret;
-	@Value("${paypal.mode}")
-	private String mode;
-	
-	// lưu lại token cho remember me
+
+	// save token for remember
 	@Bean
 	public PersistentTokenRepository persistentTokenRepository() {
 		return new InMemoryTokenRepositoryImpl();
 	}
 	
-	//lấy bộ băm bcrypt
+	// get BcryptPasswordEncoder
 	@Bean
 	public BCryptPasswordEncoder getBcryptPasswordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
 	
+	// get Instance singleton User
 	@Bean
 	public Users getUsers() {
 		return new Users();
 	}
-
-	@Bean
-	public Map<String, String> paypalSdkConfig() {
-		Map<String, String> configMap = new HashMap<>();
-		configMap.put("mode", mode);
-		return configMap;
-	}
-
-	@Bean
-	public OAuthTokenCredential oAuthTokenCredential() {
-		return new OAuthTokenCredential(clientId, clientSecret, paypalSdkConfig());
-	}
-
-	@Bean
-	public APIContext apiContext() throws PayPalRESTException {
-		APIContext context = new APIContext(oAuthTokenCredential().getAccessToken());
-		context.setConfigurationMap(paypalSdkConfig());
-		return context;
-	}
-	
 
 }
