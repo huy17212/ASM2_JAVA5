fd1d6b7121467ace3df677e57f9729f5fd95cec1
huy17212 <hahuytri2K3@gmail.com>
2023-06-09 17:27:47 +0700

    dust

--- src/main/java/com/HTT/company/util/CheckoutDtoUtils.java
+++ src/main/java/com/HTT/company/util/CheckoutDtoUtils.java
@@ -41,7 +41,7 @@ public class CheckoutDtoUtils {
 		checkoutDto.setPayment(checkOutMap.get("payment"));
 		checkoutDto.setPostcodeZip(checkOutMap.get("postcodeZip"));
 		
-		Map<String, Integer> map = new LinkedHashMap<String, Integer>();
+		Map<String, String> map = new LinkedHashMap<String, String>();
 		for(String keyValue : checkOutMap.get("mapProduct").split(" *}, *")) {
 		   String[] pairs = keyValue.split(" *= *", 2);
 		   map.put(pairs[0], pairs.length == 1 ? "" : pairs[1]);
